5557df3e697f915a9e04b8c682e515659c67c0b0
Jose Segura <jossegura2023@u.northwestern.edu>
2023-04-14 22:05:31 -0500

    added more comments to server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+// required packages 
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const { v4: uuidv4 } = require("uuid");
+const { v4: uuidv4 } = require("uuid"); // using this line of code to require the uuid package for each notes generated id
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -22,20 +23,20 @@ app.get("/api/notes", (req, res) => {
         res.json(JSON.parse(data));
     });
 });
-
+// this line is for getting all the other routes we didnt call Note: remember to put get("*") after the other routes
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
-
+// posting the notes that a user saves
 app.post("/api/notes", (req, res) => {
     const newNote = req.body;
-    newNote.id = uuidv4();
+    newNote.id = uuidv4(); // each new note will be given a unique id
 
     fs.readFile("db.json", "utf8", (err, data) => {
         if (err) throw err;
         const notes = JSON.parse(data);
         notes.push(newNote);
-
+// creates content with in the db.json file
     fs.writeFile("db.json", JSON.stringify(notes), (err) => {
         if (err) throw err;
         res.json(newNote);
@@ -48,18 +49,18 @@ app.delete("/api/notes/:id", (req, res) => {
     const noteId = req.params.id;
 
     fs.readFile("db.json", "utf8", (err, data) => {
-        if (err) throw err;
+        if (err) throw err; // if error then throws an error to let you know there's an issue
 
         let notes = JSON.parse(data);
         notes = notes.filter((note) => note.id !== noteId);
 
         fs.writeFile("db.json", JSON.stringify(notes), (err) => {
             if (err) throw err;
-            res.sendStatus(200);
+            res.sendStatus(200); // 200 status ok to ensure everything went well
         });
     });
 });
-
+// if our backend is successful then consoled message will appear when npm start is ran
 app.listen(PORT, () => {
     console.log(`Server is listening on PORT ${PORT}`);
 })
